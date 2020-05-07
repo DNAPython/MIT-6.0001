217ce7f36a4a6a240a70ddcb1bdd078edfd36b01
DNAPython <64758186+DNAPython@users.noreply.github.com>
2020-05-06 23:06:29 -0400

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# this is an example of the solution to MIT OCW problem set 0
+# https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/assignments/I
+# by DNAPython
+
+import numpy as np
+import math
+
+# use open code to set decimal places to 2
+def set_numpy_decimal_places(places, width=0):
+    set_np = '{0:' + str(width) + '.' + str(places) + 'f}'
+    np.set_printoptions(formatter={'float': lambda x: set_np.format(x)})
+
+set_numpy_decimal_places(2)
+
+#get x
+x = int(input("select your x ? >")) 
+
+#get y
+y = int(input("select your y ? >"))
+
+#raise x to the power of y
+result = (x**y)
+
+# return the result of the calculation
+print(f"your x raised to the power of y is {result}")
+
+# for some reason take the log of x in base 2
+log_result = math.log(x,2)                      
+
+# print the result
+print(f"your result log is {log_result}")
+
